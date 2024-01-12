2da1af03a0633f2a9ee0112c4173349f73def597
Its-Tanay <tantiw1910@gmail.com>
2024-01-12 16:46:41 +0530

    Initalising the project

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/api/test', async (req : Request, res: Response) => {
+    res.json({ message : "Hello World"});
+})
+
+app.listen(3001, () => {
+    console.log("Server running on port 3001");
+})
