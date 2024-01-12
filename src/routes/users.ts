4257d70bfb5c5dcbd6a9f23571c40746fad244ec
Its-Tanay <tantiw1910@gmail.com>
2024-01-12 17:45:52 +0530

    created registration endpoint

---
+++ src/routes/users.ts
@@ -0,0 +1,40 @@
+import express, {Request, Response} from "express";
+import { User } from "../models/user";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+router.post("/register", async(req: Request, res: Response) => {
+    try{
+        let user = await User.findOne({
+            email : req.body.email
+        });
+
+        if(user){
+            return res.status(400).json({message: "User already exists"});
+        }
+
+        user = new User(req.body);
+        await user.save();
+
+        const token = jwt.sign(
+            {userId: user.id}, 
+            process.env.JWT_SECRET_KEY as string, {
+                expiresIn: "1d"
+            }
+        );
+
+        res.cookie("auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 60 * 60 * 24 * 1000 
+        })
+
+        return res.sendStatus(200);
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).send({message: "Something went wrong"})
+    }
+});
+
+export default router;
